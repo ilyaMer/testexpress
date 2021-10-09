92f6a5350be2da9fe05fdef80ef057610f6051d3
Ilya Merzlyakov <ilyamerzlyakov@gmail.com>
2021-10-09 21:41:21 +0300

    cors

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 const express = require("express");
+var cors = require("cors");
 const app = express();
+
+var corsOptions = {
+  origin: "http://localhost:4000",
+};
+
+app.use(cors());
+
 const port = 3500;
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+  res.send("Hello World!");
 });
 
 app.post("/", function (req, res) {
-    res.send("POST request to the homepage");
+  res.send("POST request to the homepage");
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Example app listening at http://localhost:${port}`);
 });
